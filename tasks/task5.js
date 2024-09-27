31e03b50c97c25ff78ba970d20085799efdceaa8
forsunka340 <162183120+forsunka340@users.noreply.github.com>
2024-09-27 18:06:22 +0300

    Update task5.js

--- tasks/task5.js
+++ tasks/task5.js
@@ -3,7 +3,13 @@
 
 
 function countOccurrences(str, char) {
-  return str.split('').filter(c => c === char).length;
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+  return count;
 }
 
 // Приклади використання:
